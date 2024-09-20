2d078049078998003bb55ba8dfa78bdf369a9620
Emilio Reyes <soda480@gmail.com>
2024-09-20 12:13:58 -0700

    ci: update build workflow
    Signed-off-by: Emilio Reyes <soda480@gmail.com>

--- build.py
+++ build.py
@@ -51,4 +51,5 @@ def set_properties(project):
     project.set_property('radon_break_build_average_complexity_threshold', 4)
     project.set_property('radon_break_build_complexity_threshold', 10)
     project.set_property('bandit_break_build', True)
-    project.set_property('anybadge_exclude', 'coverage, complexity')
+    project.set_property('anybadge_exclude', 'coverage')
+    project.set_property('anybadge_complexity_use_average', True)
